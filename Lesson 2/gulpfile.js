9c0a5a8665ae4055dfdb2b0c39d935a95ffa92ab
derekmisler <derekmisler@gmail.com>
2015-12-03 17:46:21 -0500

    installed gulp-related modules

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
-	console.log('hello world!');
+	gulp.watch('sass/**/*.scss', ['styles']);
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+gulp.task('styles', function () {
+  gulp.src('sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./css'));
 });
